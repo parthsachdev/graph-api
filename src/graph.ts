f06b8a49e408fc3ea850fe9bc3fbf341f471a538
parthsachdev <parthsachdev05@gmail.com>
2023-08-12 22:08:59 +0530

    setup changes

--- src/graph.ts
+++ src/graph.ts
@@ -1,12 +1,58 @@
-class Graph {
-	private num: Number;
+/**
+ * InputStream
+ */
+export class In extends String {}
 
-	constructor(n: Number) {
-		this.num = n;
+export class Graph {
+
+	/**
+	 * Create an empty graph with V vertices OR
+	 * create a graph from empty stream
+	 * @param VOrIn V: no of vertices or Input Stream
+	 */
+	public constructor(VOrIn: Number | In) {
+		if (VOrIn instanceof Number) {
+
+		} else if (VOrIn instanceof In) {
+
+		} else {
+			throw new Error('Invalid Input');
+		}
+	}
+
+	/**
+	 * add an edge v-w
+	 */
+	addEdge(v: Number, w: Number): void {}
+
+	/**
+	 *
+	 * @param v vertex
+	 * @returns vertices adjacent to v
+	 */
+	adj(v: Number): Number[] {
+		return [];
+	}
+
+	/**
+	 * @returns number of vertices
+	 */
+	V(): Number {
+		return 0;
+	}
+
+	/**
+	 * @returns no of edges
+	 */
+	E(): Number {
+		return 0;
+	}
+
+	/** string representation */
+	toString(): String {
+		return '';
 	}
 
 }
 
-const g: Graph = new Graph(3);
-console.log(g);
 
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import {Graph, In} from './graph';
+
+
+// read graph from input stream
+const inputStream: String = '';
+const ins: In = new In();
+const graph: Graph = new Graph(ins);
+
+console.log(graph);
